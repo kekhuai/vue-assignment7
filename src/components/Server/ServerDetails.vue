e63e399e0474b53824997c7d5e77e9663e478748
Natthaphon Phanit <nulladivides@protonmail.com>
2018-06-03 13:12:20 +0700

    first commit

---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="col-xs-12 col-sm-6" v-if="selectedServer">
+        <p>Server #{{ selectedServer.id }} {{ selectedServer.status }}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: ['selectedServer']
+}
+</script>
+
+<style>
+
+</style>
